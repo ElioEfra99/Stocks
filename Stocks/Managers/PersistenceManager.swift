9842577615b822e636fe9ee336ccfe1b0388d7b4
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2022-08-01 20:55:13 -0500

    Setting up initial PersistenceManager

--- Stocks/Managers/PersistenceManager.swift
+++ Stocks/Managers/PersistenceManager.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+final class PersistenceManager {
+    static let shared = PersistenceManager()
+    private let userDefaults: UserDefaults = .standard
+    
+    struct Constants {
+        
+    }
+    
+    private init() {}
+    
+    // MARK: - Public
+    
+    public var watchList: [String] {
+        return []
+    }
+    
+    public func addToWatchList() {
+        
+    }
+    
+    public func removeFromWatchList() {
+        
+    }
+    
+    // MARK: - Private
+    
+    private var hasOnboarded: Bool {
+        return false
+    }
+}
