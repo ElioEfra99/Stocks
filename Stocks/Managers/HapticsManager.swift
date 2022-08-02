6ac39a3fc2dd007dd0609abcf995b3fb85cd916e
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2022-08-01 21:00:03 -0500

    Setting up initial HapticsManager

--- Stocks/Managers/HapticsManager.swift
+++ Stocks/Managers/HapticsManager.swift
@@ -5,4 +5,18 @@
 //  Created by Eliu Efraín Díaz Bravo on 01/08/22.
 //
 
-import Foundation
+import UIKit
+
+final class HapticsManager {
+    static let shared = HapticsManager()
+    
+    private init() { }
+    
+    // MARK: - Public
+    
+    public func vibrateForSelection() {
+        // Vibrate lightly for a selection tap interaction
+    }
+    
+    // Vibrate for type
+}
