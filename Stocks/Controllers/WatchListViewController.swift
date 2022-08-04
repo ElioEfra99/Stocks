bd2c75f5b21ba492cf6e9f0dc5a3a2efeeec94aa
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2022-08-03 21:18:45 -0500

    Added TitleView to the WatchListVC and created Extensions file

--- Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Controllers/WatchListViewController.swift
@@ -8,18 +8,41 @@
 import UIKit
 
 class WatchListViewController: UIViewController {
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setupSearchViewController()
+        setupTitleView()
     }
+    
+    // MARK: - Private
     
     private func setupSearchViewController() {
         let resultVC = SearchResultsViewController()
         let searchVC = UISearchController(searchResultsController: resultVC)
         searchVC.searchResultsUpdater = self
         navigationItem.searchController = searchVC
+    }
+    
+    private func setupTitleView() {
+        let titleView = UIView(frame: CGRect(
+            x: 0,
+            y: 0,
+            width: view.width,
+            height: navigationController?.navigationBar.height ?? 100)
+        )
+        
+        let label = UILabel(frame: CGRect(x: 10, y: 0,
+                                          width: titleView.width - 20,
+                                          height: titleView.height))
+        label.font = .systemFont(ofSize: 32, weight: .bold)
+        label.text = "Stocks"
+        titleView.addSubview(label)
+        
+        navigationItem.titleView = titleView
     }
 }
 
