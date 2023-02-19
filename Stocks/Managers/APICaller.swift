6f1c22c65d99ee4397efb2da8d1397716c8a3b06
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2023-02-18 22:28:05 -0600

    Added News API Call and async/await request

--- Stocks/Managers/APICaller.swift
+++ Stocks/Managers/APICaller.swift
@@ -26,6 +26,7 @@ final public class APICaller {
         }
         
         static let baseURL = "https://finnhub.io/api/v1/"
+        static let day: TimeInterval = 3600 * 24
     }
     
     private init() { }
@@ -53,10 +54,38 @@ final public class APICaller {
         )
     }
     
+    public func news(for type: NewsViewController.`Type`) async -> Result<[NewsStory], Error> {
+        switch type {
+        case .topStories:
+            return await asyncRequest(
+                url: url(for: .topStories, queryParam: ["category": "general"]),
+                expecting: [NewsStory].self
+            )
+        case .company(let symbol):
+            let today = Date()
+            let oneMonthAgo = today.addingTimeInterval(-(Constants.day * 7))
+            
+            return await asyncRequest(
+                url: url(
+                    for: .companyNews,
+                    queryParam: [
+                        "symbol": symbol,
+                        "from": DateFormatter.newsDateFormatter.string(from: oneMonthAgo),
+                        "to": DateFormatter.newsDateFormatter.string(from: today)
+                    ]
+                ),
+                expecting: [NewsStory].self
+            )
+        }
+        
+    }
+    
     // MARK: - Private
     
     private enum Endpoint: String {
         case search
+        case topStories = "news"
+        case companyNews = "company-news"
     }
     
     private enum APIError: Error {
@@ -123,4 +152,20 @@ final public class APICaller {
         task.resume()
     }
     
+    private func asyncRequest<T: Codable>(url: URL?, expecting: T.Type) async -> Result<T, Error> {
+        guard let url else {
+            return .failure(APIError.invalidURL)
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let decodedData = try JSONDecoder().decode(T.self, from: data)
+            
+            return .success(decodedData)
+        }
+        catch {
+            return .failure(error)
+        }
+    }
 }
--- Stocks/Other/Extensions.swift
+++ Stocks/Other/Extensions.swift
@@ -7,6 +7,28 @@
 
 import UIKit
 
+// MARK: - Date Formatter
+
+extension DateFormatter {
+    static let newsDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY-MM-dd"
+        return formatter
+    }()
+}
+
+// MARK: - Add Subview
+
+extension UIView {
+    func addSubviews(_ view: UIView...) {
+        view.forEach {
+            addSubview($0)
+        }
+    }
+}
+
+// MARK: - Framing
+
 extension UIView {
     var width: CGFloat {
         frame.size.width
@@ -19,15 +41,17 @@ extension UIView {
     var top: CGFloat {
         frame.origin.y
     }
+    
     var left: CGFloat {
         frame.origin.x
     }
+    
     var right: CGFloat {
         left + width
     }
+    
     var bottom: CGFloat {
         top + height
     }
-    
 }
 
